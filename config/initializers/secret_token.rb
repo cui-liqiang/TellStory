d7df35e6d6239c0083efc467e11039e99dc923ac
Cui Liqiang <cui.liqiang@gmail.com>
2012-04-22 09:48:38 +0800

    init with the static html and assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TellStory::Application.config.secret_token = '101b645cda9c9eccea716db7c3d3c2410b4f6b8eea7738e8bbf87c72fb88f9d6a8d875dbc702c677a67e2ed1f4ec36116d8eb05ef34e4225aac8529444ab5e61'
