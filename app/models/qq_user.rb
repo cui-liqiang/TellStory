2595837f76420f6939157376f18b9e5162aa0633
Cui Liqiang <cui.liqiang@gmail.com>
2012-05-27 23:02:39 +0800

    rename users table to qq_users. functionality is broken now..

--- app/models/qq_user.rb
+++ app/models/qq_user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class QQUser < ActiveRecord::Base
   attr_accessible :display_name, :name, :head
 
   has_many :stories
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120520073534) do
+ActiveRecord::Schema.define(:version => 20120527143502) do
 
   create_table "comments", :force => true do |t|
     t.text     "content"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(:version => 20120520073534) do
     t.integer  "user_id"
   end
 
+  create_table "qq_users", :force => true do |t|
+    t.string   "name"
+    t.string   "display_name"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+    t.string   "head"
+  end
+
   create_table "stories", :force => true do |t|
     t.string   "title",                        :null => false
     t.text     "content",                      :null => false
@@ -40,14 +48,6 @@ ActiveRecord::Schema.define(:version => 20120520073534) do
     t.datetime "updated_at",                   :null => false
     t.datetime "round_time"
     t.integer  "user_id"
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "display_name"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-    t.string   "head"
   end
 
   create_table "votes", :force => true do |t|
