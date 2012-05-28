01fb3f5e247cbadb51be9f9d6a655b3298a4b769
Cui Liqiang <cui.liqiang@gmail.com>
2012-05-28 23:40:34 +0800

    finish backend logic handling registration and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,34 @@
+# encoding: utf-8
 class UsersController < ApplicationController
-  def new
+  def create
+    if params[:commit] == "登陆"
+      handle_login
+    else
+      handle_register
+    end
+  end
 
+  private
+
+  def handle_register
+    user = User.new
+    user.email = params[:user][:email]
+    user.password = params[:user][:password]
+    user.save
+    session[:user] = user
+    session[:logged_in] = true
+    redirect_to "/"
+  end
+
+  def handle_login
+    @user = User.authenticate(params[:user][:email], params[:user][:password])
+    if @user.nil?
+      @user = User.new
+      redirect_to "/"
+    else
+      session[:user] = @user
+      session[:logged_in] = true
+      redirect_to "/"
+    end
   end
 end
