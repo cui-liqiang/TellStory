ab6cf1ac9e3c0dabf1ebd7c955c2b1726cda1ad3
Cui Liqiang <cui.liqiang@gmail.com>
2012-06-17 15:34:33 +0800

    remove the rspec test because they are no longger useful

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,10 @@ FactoryGirl.define do
     password 'password'
     password_confirmation 'password'
   end
+
+  factory :sina_user do
+    sina_id 'sina id'
+    display_name 'liqiang'
+    head 'head/link'
+  end
 end
